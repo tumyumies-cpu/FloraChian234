86f4cfdf4cbd5caed80a8aef8c7223858d8ee0d8
tum yum <tumyumies@gmail.com>
2025-09-21 04:01:51 +0000

    see the image they are mixing each other

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -5,20 +5,18 @@ import { Sidebar, SidebarProvider, SidebarInset } from '@/components/ui/sidebar'
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <div className="min-h-screen">
-        <Sidebar
-          variant="sidebar"
-          collapsible="icon"
-          className="border-r border-border/80"
-        >
-          <SidebarNav />
-        </Sidebar>
-        <SidebarInset>
-          <MainHeader />
-          <main className="p-4 sm:p-6 lg:p-8">
-            {children}
-          </main>
-        </SidebarInset>
+      <Sidebar
+        variant="sidebar"
+        collapsible="icon"
+        className="border-r border-border/80"
+      >
+        <SidebarNav />
+      </Sidebar>
+      <div className="flex flex-col w-full">
+        <MainHeader />
+        <main className="p-4 sm:p-6 lg:p-8">
+          {children}
+        </main>
       </div>
     </SidebarProvider>
   );
