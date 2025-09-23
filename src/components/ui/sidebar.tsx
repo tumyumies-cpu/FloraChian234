cf7ef3c1fb1718ae56c6536c7dbe396a533ccc32
tum yum <tumyumies@gmail.com>
2025-09-23 11:44:50 +0000

    still smae

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -198,7 +198,7 @@ const Sidebar = React.forwardRef<
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
-            className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
+            className="w-[--sidebar-width] bg-white text-black p-0 [&>button]:hidden"
             style={
               {
                 "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
