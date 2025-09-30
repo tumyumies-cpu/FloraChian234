8d80f437a18f3be32a9b2978880fc90b26dc1dd7
tum yum <tumyumies@gmail.com>
2025-09-30 03:13:22 +0000

    fix the ai issue and for the judge point of view make it wokring

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,5 +4,5 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'googleai/gemini-pro',
+  model: 'googleai/gemini-1.5-flash-latest',
 });
