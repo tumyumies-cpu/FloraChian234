0263303a5e9f17d8b0416eeea4c885246d18ec0d
tum yum <tumyumies@gmail.com>
2025-09-30 02:47:41 +0000

    Error occurred prerendering page "/login". Read more: https://nextjs.org

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useState } from 'react';
+import { useState, Suspense } from 'react';
 import { useRouter } from 'next/navigation';
 import { useAuth } from '@/context/auth-context';
 import { Button } from '@/components/ui/button';
